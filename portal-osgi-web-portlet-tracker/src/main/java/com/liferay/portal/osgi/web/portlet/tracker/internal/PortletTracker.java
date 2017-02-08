NolanChan/liferay-portal

LPS-69811 Replace periods and dollar signs with underscores first

modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -121,10 +121,10 @@ public class PortletTracker
 			portletName = clazz.getName();
 		}
 
-		String portletId = PortalUtil.getJsSafePortletId(portletName);
+		String portletId = StringUtil.replace(
+			portletName, new char[] {'.', '$'}, new char[] {'_', '_'});
 
-		portletId = StringUtil.replace(
-			portletId, new char[] {'$'}, new char[] {'_'});
+		portletId = PortalUtil.getJsSafePortletId(portletId);
 
 		if (portletId.length() >
 				PortletInstance.PORTLET_INSTANCE_KEY_MAX_LENGTH) {

johnplatts/com-liferay-portal-osgi-web

portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -121,10 +121,10 @@ public class PortletTracker
 			portletName = clazz.getName();
 		}
 
-		String portletId = _portal.getJsSafePortletId(portletName);
+		String portletId = StringUtil.replace(
+			portletName, new char[] {'.', '$'}, new char[] {'_', '_'});
 
-		portletId = StringUtil.replace(
-			portletId, new char[] {'$'}, new char[] {'_'});
+		portletId = _portal.getJsSafePortletId(portletId);
 
 		if (portletId.length() >
 				PortletInstance.PORTLET_INSTANCE_KEY_MAX_LENGTH) {